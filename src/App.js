04078307b2410a8435b3eff792b8f47c4580dcc2
teikim98 <teikim98@gmail.com>
2023-01-19 15:12:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef, useCallback } from "react";
+import { socket, SocketContext, SOCKET_EVENT } from "./service/socket";
+import NicknameForm from "./components/NicknameForm";
+import ChatRoom from "./components/ChatRoom";
 
 function App() {
+  const prevNickname = useRef(null);
+  const [nickname, setNickname] = useState("김첨지");
+
+  useEffect(() => {
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.emit(SOCKET_EVENT.JOIN_ROOM, { nickname }); // JOIN_ROOM event type과 nickname data를 서버에 전송한다.
+  }, [nickname]);
+
+  useEffect(() => {
+    if (prevNickname.current) {
+      socket.emit(SOCKET_EVENT.UPDATE_NICKNAME, {
+        // 서버에는 이전 닉네임과 바뀐 닉네임을 전송해줍니다.
+        prevNickname: prevNickname.current,
+        nickname,
+      });
+    } else {
+      socket.emit(SOCKET_EVENT.JOIN_ROOM, { nickname });
+    }
+  }, [nickname]);
+
+  const handleSubmitNickname = useCallback(
+    (newNickname) => {
+      prevNickname.current = nickname;
+      setNickname(newNickname);
+    },
+    [nickname]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SocketContext.Provider value={socket}>
+      <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+        <NicknameForm handleSubmitNickname={handleSubmitNickname} />
+        <ChatRoom nickname={nickname} />
+      </div>
+    </SocketContext.Provider>
   );
 }
 
